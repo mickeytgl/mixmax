9fc922e4be22021c0e43025567380b38340cea6d
Miguel Torres <migueltg93@gmail.com>
2019-08-21 16:01:05 -0500

    Bump version

--- lib/mixmax.rb
+++ lib/mixmax.rb
@@ -1,5 +1,5 @@
-require_relative "mixmax/version"
-require "httparty"
+require_relative 'mixmax/version'
+require 'httparty'
 
 class Mixmax
   include HTTParty
@@ -7,46 +7,48 @@ class Mixmax
   attr_reader :headers
 
   def initialize(api_key)
-    @headers = { "X-API-Token" => api_key, "Content-Type" => "application/json"}
+    @headers = {
+      'X-API-Token' => api_key, 'Content-Type' => 'application/json'
+    }
   end
 
-  base_uri "https://api.mixmax.com"
+  base_uri 'https://api.mixmax.com'
 
-    def sequences(query: {})
-      get("/v1/sequences", query: query)["results"]
-    end
+  def sequences(query: {})
+    get('/v1/sequences', query: query)['results']
+  end
 
-    def sequence(id)
-      get "/v1/sequences/#{id}"
-    end
+  def sequence(id)
+    get "/v1/sequences/#{id}"
+  end
 
-    def sequence_recipients(id)
-      get "/v1/sequences/#{id}/recipients"
-    end
+  def sequence_recipients(id)
+    get "/v1/sequences/#{id}/recipients"
+  end
 
-    def add_to_sequence(id, recipients:)
-      post "/v1/sequences/#{id}/recipients", query: recipients
-    end
+  def add_to_sequence(id, recipients:)
+    post "/v1/sequences/#{id}/recipients", query: recipients
+  end
 
-    def user_preferences
-      get "/v1/userpreferences/me"
-    end
+  def user_preferences
+    get '/v1/userpreferences/me'
+  end
 
-    def change_user_preferences(query: {})
-      patch "/v1/userpreferences/me", body: query
-    end
+  def change_user_preferences(query: {})
+    patch '/v1/userpreferences/me', body: query
+  end
 
-    def remove_recipient_from_sequence(sequence_id:, recipient_id:)
-      post "/sequences/#{sequence_id}/recipients/#{recipient_id}/cancel"
-    end
+  def remove_recipient_from_sequence(sequence_id:, recipient_id:)
+    post "/sequences/#{sequence_id}/recipients/#{recipient_id}/cancel"
+  end
 
-    private
+  private
 
-    def get(route, query: {})
-      self.class.get(route, headers: headers, body: query).parsed_response
-    end
+  def get(route, query: {})
+    self.class.get(route, headers: headers, body: query).parsed_response
+  end
 
-    def post(route, query: {})
-      self.class.post(route, headers: headers, body: query)
-    end
+  def post(route, query: {})
+    self.class.post(route, headers: headers, body: query)
+  end
 end
