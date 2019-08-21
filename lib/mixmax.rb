424859a5680cde21fd9d9a7be73a5fdce8479581
Miguel Torres <migueltg93@gmail.com>
2019-08-21 16:00:43 -0500

    Move repeatable logic to private methods, add remove_recipient_from_sequence method

--- lib/mixmax.rb
+++ lib/mixmax.rb
@@ -4,25 +4,49 @@ require "httparty"
 class Mixmax
   include HTTParty
 
+  attr_reader :headers
+
   def initialize(api_key)
     @headers = { "X-API-Token" => api_key, "Content-Type" => "application/json"}
   end
 
   base_uri "https://api.mixmax.com"
 
-  def sequences(query = {})
-    self.class.get("/v1/sequences", headers: @headers, query: query).parsed_response["results"]
-  end
+    def sequences(query: {})
+      get("/v1/sequences", query: query)["results"]
+    end
 
-  def sequence(id)
-    self.class.get("/v1/sequences/#{id}", headers: @headers).parsed_response
-  end
+    def sequence(id)
+      get "/v1/sequences/#{id}"
+    end
 
-  def sequence_recipients(id)
-    self.class.get("/v1/sequences/#{id}/recipients", headers: @headers)
-  end
+    def sequence_recipients(id)
+      get "/v1/sequences/#{id}/recipients"
+    end
 
-  def add_to_sequence(id, recipients)
-    self.class.post("/v1/sequences/#{id}/recipients", headers: @headers, body: recipients)
-  end
+    def add_to_sequence(id, recipients:)
+      post "/v1/sequences/#{id}/recipients", query: recipients
+    end
+
+    def user_preferences
+      get "/v1/userpreferences/me"
+    end
+
+    def change_user_preferences(query: {})
+      patch "/v1/userpreferences/me", body: query
+    end
+
+    def remove_recipient_from_sequence(sequence_id:, recipient_id:)
+      post "/sequences/#{sequence_id}/recipients/#{recipient_id}/cancel"
+    end
+
+    private
+
+    def get(route, query: {})
+      self.class.get(route, headers: headers, body: query).parsed_response
+    end
+
+    def post(route, query: {})
+      self.class.post(route, headers: headers, body: query)
+    end
 end
