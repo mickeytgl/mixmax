37dfaffc76038b34570e2533b7c9c9980333a4df
Miguel Torres <migueltg93@gmail.com>
2019-09-02 12:38:39 -0500

    Fixed remove_recipients_from_sequence methods. Convert to json before sending the query

--- lib/mixmax.rb
+++ lib/mixmax.rb
@@ -38,17 +38,17 @@ class Mixmax
     patch '/v1/userpreferences/me', body: query
   end
 
-  def remove_recipient_from_sequence(sequence_id:, recipient_id:)
-    post "/sequences/#{sequence_id}/recipients/#{recipient_id}/cancel"
+  def remove_recipient_from_sequence(sequence_id:, emails:)
+    post "/v1/sequences/#{sequence_id}/cancel", query: { emails: Array(emails) }
   end
 
   private
 
   def get(route, query: {})
-    self.class.get(route, headers: headers, body: query).parsed_response
+    self.class.get(route, headers: headers, body: query.to_json).parsed_response
   end
 
   def post(route, query: {})
-    self.class.post(route, headers: headers, body: query)
+    self.class.post(route, headers: headers, body: query.to_json)
   end
 end
